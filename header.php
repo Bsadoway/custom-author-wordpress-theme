8408e0f53c72d37fa2b345c547513b7d1e6a2319
Blake Sadoway <BlakeSadoway@gmail.com>
2018-06-12 14:15:36 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php
+bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
+	
+		
+</head>
---
+++ navigation.php
@@ -0,0 +1,36 @@
+
+<div id="wrapper">
+
+<!-- navigation -->
+
+<div id="header">
+
+	<div id="navigation">
+		<ul class="nav">
+			<li><a class="nav-link" href="#about">ABOUT</a></li>
+			<li><a class="nav-link" href="#in-person-full">IN PERSON</a></li>
+			<li><a class="nav-link" href="#books">BOOKS</a></li>
+			<li><a class="nav-link" href="#contact-full">CONTACT</a></li>
+		</ul>
+	</div>
+	<!-- END navigation-->
+	
+	<div id="socmed">
+		<div id="socmed-flags">
+		<ul class="socmed-list">
+			<li class="tumblr"><a href="http://instagram.com/rkdelaney"><img src="<?php bloginfo('stylesheet_directory');?>/images/instagram-flag.png" class="instagram-flag" title="Instagram" width="45px" /></a></li>
+			<li class="twitter"><a href="http://twitter.com/rkdelaney"><img src="<?php bloginfo('stylesheet_directory');?>/images/twitter-flag.png" class="twitter-flag" title="Twitter" width="45px" /></a></li>
+			<li class="good-reads"><a href="http://www.goodreads.com/author/show/2879536.Rachelle_Delaney"><img src="<?php bloginfo('stylesheet_directory');?>/images/goodreads-flag.png" class="goodreads-flag" title="Good Reads" width="45px" /></a></li>
+		</ul>
+		</div>
+		<!-- END socmed-flags -->
+	</div>
+	<!-- END socmed-->
+
+	
+	
+	
+	
+
+</div>
+<!-- END header-->
