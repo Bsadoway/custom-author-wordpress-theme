46159526c4c4685a3c4ea4509ca42a85d459f4ec
Blake Sadoway <BlakeSadoway@gmail.com>
2018-06-20 20:50:09 -0700

    fixed feature book image for mobile

--- homepage.php
+++ homepage.php
@@ -3,12 +3,12 @@
 
 <div class="container">
     <div class="row align-items-center">
-        <div class="col">
+        <div class="col-sm">
             <?php $image = get_field('feature_book_image'); ?>
             <img class="img-border" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
         </div>
 
-        <div class="col">
+        <div class="col-sm">
             <h2>
                 <?php the_field('feature_book_title');?>
             </h2>
@@ -20,7 +20,7 @@
             </p>
 
         </div>
-        <div class="col">
+        <div class="col-sm">
             <div class="img-border"></div>
         </div>
     </div>
