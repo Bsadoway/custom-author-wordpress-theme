96e4f9c2af8fe3efbef26500161962f750130614
Blake Sadoway <BlakeSadoway@gmail.com>
2018-06-21 13:59:14 -0700

    updated the books into bootstrap cards

--- homepage.php
+++ homepage.php
@@ -2,7 +2,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="row align-items-center">
+    <div class="row align-items-top">
         <div class="col-sm">
             <?php $image = get_field('feature_book_image'); ?>
             <img class="img-padding" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
@@ -21,26 +21,46 @@
         </div>
 
         <div class="col-sm">
-            <div>
+            <div class="other-books-container">
                 <h2 class="other-books-title">Other Books</h2>
                 <ul class="book-list">
                     <li class="book-thumbnail">
-                        <?php $imageThumb1 = get_field('book1_thumb'); ?>
-                        <img class="img-padding" src="<?php echo $imageThumb1['url']; ?>" alt="<?php echo $imageThumb1['alt']; ?>" />
+                        <div class="card">
+                            <?php $imageThumb1 = get_field('book1_thumb'); ?>
+                            <img class="img-padding card-img-top" src="<?php echo $imageThumb1['url']; ?>" alt="<?php echo $imageThumb1['alt']; ?>" />
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <?php the_field('book1_title'); ?>
+                                </p>
+                            </div>
+                        </div>
                     </li>
                     <li class="book-thumbnail">
-                        <?php $imageThumb2 = get_field('book2_thumb'); ?>
-                        <img class="img-padding" src="<?php echo $imageThumb2['url']; ?>" alt="<?php echo $imageThumb2['alt']; ?>" />
+                        <div class="card">
+                            <?php $imageThumb2 = get_field('book2_thumb'); ?>
+                            <img class="img-padding card-img-top" src="<?php echo $imageThumb2['url']; ?>" alt="<?php echo $imageThumb2['alt']; ?>" />
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <?php the_field('book2_title'); ?>
+                                </p>
+                            </div>
+                        </div>
                     </li>
                     <li class="book-thumbnail">
-                        <?php $imageThumb3 = get_field('book3_thumb'); ?>
-                        <img class="img-padding" src="<?php echo $imageThumb3['url']; ?>" alt="<?php echo $imageThumb3['alt']; ?>" />
+                        <div class="card">
+                            <?php $imageThumb3 = get_field('book3_thumb'); ?>
+                            <img class="img-padding card-img-top" src="<?php echo $imageThumb3['url']; ?>" alt="<?php echo $imageThumb3['alt']; ?>" />
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <?php the_field('book3_title'); ?>
+                                </p>
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-
 </div>
 
 <?php get_footer(); ?>
