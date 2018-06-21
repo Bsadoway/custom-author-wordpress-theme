9354aa39659589a16f80547b26d7c1c73ab89063
Blake Sadoway <BlakeSadoway@gmail.com>
2018-06-20 22:24:53 -0700

    added other books column and enable basic mobile responsiveness

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@
     <div class="row align-items-center">
         <div class="col-sm">
             <?php $image = get_field('feature_book_image'); ?>
-            <img class="img-border" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+            <img class="img-padding" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
         </div>
 
         <div class="col-sm">
@@ -18,10 +18,26 @@
             <p>
                 <?php the_field('paragraph_text');?>
             </p>
-
         </div>
+
         <div class="col-sm">
-            <div class="img-border"></div>
+            <div>
+                <h2 class="other-books-title">Other Books</h2>
+                <ul class="book-list">
+                    <li class="book-thumbnail">
+                        <?php $imageThumb1 = get_field('book1_thumb'); ?>
+                        <img class="img-padding" src="<?php echo $imageThumb1['url']; ?>" alt="<?php echo $imageThumb1['alt']; ?>" />
+                    </li>
+                    <li class="book-thumbnail">
+                        <?php $imageThumb2 = get_field('book2_thumb'); ?>
+                        <img class="img-padding" src="<?php echo $imageThumb2['url']; ?>" alt="<?php echo $imageThumb2['alt']; ?>" />
+                    </li>
+                    <li class="book-thumbnail">
+                        <?php $imageThumb3 = get_field('book3_thumb'); ?>
+                        <img class="img-padding" src="<?php echo $imageThumb3['url']; ?>" alt="<?php echo $imageThumb3['alt']; ?>" />
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
