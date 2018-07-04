94478ec4ec5304a332cf450d9393303bb419a635
Blake Sadoway <BlakeSadoway@gmail.com>
2018-07-03 19:50:03 -0700

    added footer content

--- footer.php
+++ footer.php
@@ -1,22 +1,23 @@
 <?php wp_footer(); ?>
-<div class="footer-row">
+<footer class="footer-row">
     <div class="container">
         <div class="row">
             <div class="col-sm footer-col">
                 <img src="<?php bloginfo('stylesheet_directory'); ?>/images/rachelle-logo-red-small.png" alt="" />
             </div>
             <div class="col-sm footer-col">
-                <?php the_field('contact-list') ?>
+                <h4>Contact Rachelle</h4>
+                <p>email@email.com</p>
             </div>
             <div class="col-sm footer-col">
                 <div class="" id="navbarSupportedContent">
                     <?php wp_nav_menu(array(
 					'menu'              => 'social2',
     				'theme_location' 	=> 'social_footer',
-    				'menu_class' 		=> 'navbar-nav nav-main ml-auto',
+    				'menu_class' 		=> 'navbar-nav',
 					'depth'             => 2,
 					'container'         => 'div',
-					'container_class'   => 'ml-auto',
+					'container_class'   => '',
 					'container_id'      => '',
     				'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
     				'walker' 			=> new wp_bootstrap_navwalker(),
@@ -25,7 +26,7 @@
             </div>
         </div>
     </div>
-</div>
+</footer>
 
 </body>
 
