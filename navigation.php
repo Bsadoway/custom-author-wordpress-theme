3ade55ed656ae0386d0175fa994bf178b05ad2ce
Blake Sadoway <BlakeSadoway@gmail.com>
2018-07-19 11:48:18 -0700

    changed header back to old style

--- navigation.php
+++ navigation.php
@@ -1,9 +1,9 @@
 <div class="nav-container">
 	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light">
 		<a class="nav-logo" href="/">
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/rachelle-logo.png" alt="" />
 		</a>
-		<nav class="navbar navbar-expand-lg navbar-light">
 			<button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 			    aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
